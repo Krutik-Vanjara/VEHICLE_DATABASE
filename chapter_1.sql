57c1010799b177e118555a6fc5dfd4c5b305f91d
Krutik Vanjara <160028087+Krutik-Vanjara@users.noreply.github.com>
2024-12-10 14:31:26 +0100

    Update chapter_1.sql

--- chapter_1.sql
+++ chapter_1.sql
@@ -6,9 +6,9 @@ GRANT DBA TO C##car_schema;
 
 ALTER SESSION SET CURRENT_SCHEMA = C##car_schema;
 
-
+-- Create table 1 cars_dataset
 CREATE TABLE CarDetails (
-    Car_ID NUMBER PRIMARY KEY NOT NULL,
+    Car_ID NUMBER PRIMARY KEY NOT NULL, -- set Car_ID as a PRIMARY KEY for this table
     Make VARCHAR2(50) NOT NULL,
     Model VARCHAR2(50) NOT NULL,
     Price NUMBER NOT NULL CHECK (Price >= 0),
@@ -17,9 +17,9 @@ CREATE TABLE CarDetails (
     Location VARCHAR2(20) NOT NULL,
     Transmission VARCHAR2(20) NOT NULL
 );
-
+-- create table 2 specification_datasets 
 CREATE TABLE CarSpecifications (
-    Spec_ID NUMBER PRIMARY KEY,
+    Spec_ID NUMBER PRIMARY KEY, -- SET  Spec_ID  AS a  PRIMARY KEY for this table2 
     Car_ID NUMBER,
     Engine VARCHAR2(50),
     Max_Power VARCHAR2(50),
@@ -28,9 +28,9 @@ CREATE TABLE CarSpecifications (
     CONSTRAINT fk_CarDetails FOREIGN KEY (Car_ID)
     REFERENCES CarDetails(Car_ID)
 );
-
+-- create table 3 features_dataset
 CREATE TABLE CarFeatures (
-    Feature_ID NUMBER PRIMARY KEY,
+    Feature_ID NUMBER PRIMARY KEY, -- Feature_ID as a PRIMARY KEY FOR THIS TABLE3
     Car_ID NUMBER,
     Color VARCHAR2(30),
     Seating_Capacity NUMBER,
@@ -38,9 +38,9 @@ CREATE TABLE CarFeatures (
     CONSTRAINT fk_Car_Details FOREIGN KEY (Car_ID)
     REFERENCES CarDetails(Car_ID)
 );
-
+-- create table 4 ownership_datasets
 CREATE TABLE CarOwnership (
-    Ownership_ID NUMBER PRIMARY KEY,
+    Ownership_ID NUMBER PRIMARY KEY, --  Ownership_ID as a PRIMARY KEY for this table4
     Car_ID NUMBER,
     Owner VARCHAR2(50),
     Seller_Type VARCHAR2(30),
@@ -48,9 +48,9 @@ CREATE TABLE CarOwnership (
     CONSTRAINT fk_CarD FOREIGN KEY (Car_ID)
     REFERENCES CarDetails(Car_ID)
 );
-
+-- create table 5 dimensions_datasets
 CREATE TABLE CarDimensions (
-    Dimension_ID NUMBER PRIMARY KEY,
+    Dimension_ID NUMBER PRIMARY KEY, --set Dimension_ID as a PRIMARY KEY for this table5
     Car_ID NUMBER,
     Length NUMBER,
     Width NUMBER,
